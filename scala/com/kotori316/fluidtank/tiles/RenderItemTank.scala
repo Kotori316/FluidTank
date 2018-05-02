ee5829bae1a7d90920b589b8fe0856db903c78a0
Kotori316 <Kotori316@gmail.com>
2018-05-02 22:16:00 +0900

    Fixed item scale.

--- scala/com/kotori316/fluidtank/tiles/RenderItemTank.scala
+++ scala/com/kotori316/fluidtank/tiles/RenderItemTank.scala
@@ -1,8 +1,8 @@
 package com.kotori316.fluidtank.tiles
 
+import com.kotori316.fluidtank.FluidTank
 import com.kotori316.fluidtank.items.ItemBlockTank
 import com.kotori316.fluidtank.packet.ClientProxy
-import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.client.renderer.tileentity.TileEntityItemStackRenderer
 import net.minecraft.item.ItemStack
 import net.minecraftforge.fml.relauncher.{Side, SideOnly}
@@ -15,18 +15,15 @@ class RenderItemTank extends TileEntityItemStackRenderer {
     override def renderByItem(stack: ItemStack, partialTicks: Float) = {
         stack.getItem match {
             case tankItem: ItemBlockTank =>
-                val compound = stack.getSubCompound("BlockEntityTag")
                 tileTank.tier = tankItem.blockTank.getTierByMeta(stack.getMetadata)
                 tileTank.tank.setFluid(null)
+                val compound = stack.getSubCompound("BlockEntityTag")
                 if (compound != null)
                     tileTank.readFromNBT(compound)
 
-                GlStateManager.pushMatrix()
-                //                GlStateManager.scale(-.5F, .5F, .5F)
                 ClientProxy.RENDER_TANK.render(tileTank, 0d, 0d, 0d, partialTicks, -1, 1.0f)
-                GlStateManager.popMatrix()
 
-            case _ =>
+            case _ => FluidTank.LOGGER.info("RenderItemTank is called for " + stack.getItem)
         }
     }
 }
