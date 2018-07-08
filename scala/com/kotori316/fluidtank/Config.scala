3ece3ce9f5e5e06ca0dca915dbc4d8d0561f0bd8
Kotori316 <Kotori316@gmail.com>
2018-07-08 23:37:44 +0900

    Added Config gui

--- scala/com/kotori316/fluidtank/Config.scala
+++ scala/com/kotori316/fluidtank/Config.scala
@@ -8,7 +8,7 @@ import net.minecraft.client.gui.GuiScreen
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.common.config.{ConfigElement, Configuration}
 import net.minecraftforge.fml.client.IModGuiFactory
-import net.minecraftforge.fml.client.config.IConfigElement
+import net.minecraftforge.fml.client.config.{DummyConfigElement, IConfigElement}
 import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent
 import net.minecraftforge.fml.relauncher.{Side, SideOnly}
 
@@ -36,7 +36,10 @@ object Config {
     }
 
     def getElements: java.util.List[IConfigElement] = {
-        new ConfigElement(configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements
+        val elements = new ConfigElement(configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements
+        elements.add(new DummyConfigElement.DummyCategoryElement("Recipe Setting", FluidTank.modID + "." + CATEGORY_RECIPE,
+            new ConfigElement(configuration.getCategory(CATEGORY_RECIPE)).getChildElements))
+        elements
     }
 
     class Content {
@@ -53,10 +56,12 @@ object Config {
         val oreNameMap: Map[Tiers, String] = Tiers.list.drop(2).map(tier => {
             val property = configuration.get(CATEGORY_RECIPE, tier + "OreName", tier.oreName)
             property.setRequiresMcRestart(true)
+            property.setComment(s"Set oredict name of items to craft $tier tank.")
             (tier, property.getString)
         }).toMap + (Tiers.Invalid -> "Unknown") + (Tiers.WOOD -> "logWood")
 
         private val showInvisibleTankProperty = configuration.get(Configuration.CATEGORY_GENERAL, "showInvisibleTankInTab", false)
+        showInvisibleTankProperty.setRequiresMcRestart(true)
         showInvisibleTankProperty.setComment("True to show invisible tank in creative tabs. Recipe and block aren't removed.")
         val showInvisibleTank = showInvisibleTankProperty.getBoolean
 
