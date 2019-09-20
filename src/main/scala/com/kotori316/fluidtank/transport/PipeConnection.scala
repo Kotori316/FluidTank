85f9d33aafa7e000877a5706043a16518cbf9337
Kotori316 <Kotori316@gmail.com>
2019-09-21 01:23:20 +0900

    Working on pipe connection

--- src/main/scala/com/kotori316/fluidtank/transport/PipeConnection.scala
+++ src/main/scala/com/kotori316/fluidtank/transport/PipeConnection.scala
@@ -1,3 +1,24 @@
 package com.kotori316.fluidtank.transport
 
-case class PipeConnection()
+import com.kotori316.scala_lib.util.Norm
+import net.minecraft.util.math.Vec3i
+
+case class PipeConnection[A: Norm](poses: Set[A], updateFunc: (A, PipeConnection[A]) => Unit) {
+  poses.foreach(p => updateFunc.apply(p, this))
+
+  def add(a: A) = PipeConnection(poses + a, updateFunc)
+
+  def remove(a: A): Unit = {
+    val empty = PipeConnection.empty(updateFunc)
+    poses.foreach(p => updateFunc.apply(p, empty))
+  }
+
+  def isEmpty = poses.isEmpty
+}
+
+object PipeConnection {
+  def empty[A: Norm](updateFunc: (A, PipeConnection[A]) => Unit): PipeConnection[A] = PipeConnection(Set.empty, updateFunc)
+
+  implicit val vec3iL2Norm: Norm[Vec3i] = v => Math.sqrt(v.getX * v.getX + v.getY * v.getY + v.getZ * v.getZ)
+  val vec3iL1Norm: Norm[Vec3i] = v => v.getX + v.getY + v.getZ
+}
