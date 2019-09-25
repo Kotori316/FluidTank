2fefbaf144d7c0dd29fc2d7447c673c4626abfa2
Kotori316 <Kotori316@gmail.com>
2019-09-25 17:22:50 +0900

    Require group instance to get the distance from a specific coord.

--- src/main/scala/com/kotori316/fluidtank/transport/PipeConnection.scala
+++ src/main/scala/com/kotori316/fluidtank/transport/PipeConnection.scala
@@ -1,12 +1,19 @@
 package com.kotori316.fluidtank.transport
 
+import cats._
+import cats.implicits._
+import cats.kernel.CommutativeGroup
 import com.kotori316.scala_lib.util.Norm
-import net.minecraft.util.math.Vec3i
+import com.kotori316.scala_lib.util.Norm._
+import net.minecraft.util.math.{BlockPos, Vec3i}
 
-case class PipeConnection[A: Norm](poses: Set[A], updateFunc: (A, PipeConnection[A]) => Unit, isOutput: A => Boolean) {
+case class PipeConnection[A](poses: Set[A], updateFunc: (A, PipeConnection[A]) => Unit, isOutput: A => Boolean)
+                            (implicit norm: Norm[A], group: Group[A]) {
   poses.foreach(p => updateFunc.apply(p, this))
 
   def outputs: Seq[A] = poses.filter(isOutput).toSeq
+
+  def outputSorted(origin: A): List[A] = outputs.sortBy(p => (p |-| origin).norm).toList
 
   def add(a: A): PipeConnection[A] = this.copy(poses = poses + a)
 
@@ -21,8 +28,18 @@ case class PipeConnection[A: Norm](poses: Set[A], updateFunc: (A, PipeConnection
 }
 
 object PipeConnection {
-  def empty[A: Norm](updateFunc: (A, PipeConnection[A]) => Unit, isOutput: A => Boolean): PipeConnection[A] = PipeConnection(Set.empty, updateFunc, isOutput)
+  def empty[A](updateFunc: (A, PipeConnection[A]) => Unit, isOutput: A => Boolean)(implicit norm: Norm[A], group: Group[A]): PipeConnection[A] = PipeConnection(Set.empty, updateFunc, isOutput)
 
   implicit val vec3iL2Norm: Norm[Vec3i] = v => Math.sqrt(v.getX * v.getX + v.getY * v.getY + v.getZ * v.getZ)
   val vec3iL1Norm: Norm[Vec3i] = v => v.getX + v.getY + v.getZ
+  implicit val posGroup: CommutativeGroup[BlockPos] = new CommutativeGroup[BlockPos] {
+    override def inverse(a: BlockPos): BlockPos = new BlockPos(-a.getX, -a.getY, -a.getZ)
+
+    override def empty: BlockPos = BlockPos.ZERO
+
+    override def remove(x: BlockPos, y: BlockPos): BlockPos = new BlockPos(x.getX - y.getX, x.getY - y.getY, x.getZ - y.getZ)
+
+    override def combine(x: BlockPos, y: BlockPos): BlockPos = new BlockPos(x.getX + y.getX, x.getY + y.getY, x.getZ + y.getZ)
+  }
+  implicit val vec3iGroup: CommutativeGroup[Vec3i] = posGroup.imap[Vec3i](identity)(new BlockPos(_))
 }
--- src/test/scala/com/kotori316/fluidtank/transport/PipeConnectionTest.scala
+++ src/test/scala/com/kotori316/fluidtank/transport/PipeConnectionTest.scala
@@ -1,5 +1,6 @@
 package com.kotori316.fluidtank.transport
 
+import cats.implicits.{catsKernelStdGroupForInt, catsKernelStdGroupForTuple2}
 import com.kotori316.fluidtank.transport.PipeConnection._
 import com.kotori316.fluidtank.transport.PipeConnectionTest.Holder
 import com.kotori316.scala_lib.util.Norm._
@@ -24,6 +25,7 @@ class PipeConnectionTest {
       t(0, 2) -> new Holder,
       t(1, 2) -> new Holder,
       t(1, 3) -> new Holder,
+      t(2, 3) -> new Holder,
     )
     val c1 = PipeConnection[(Int, Int)](Set(t(0, 1)), { case (p, c) => map.get(p).foreach(_.setConnection(c)) }, _ => false)
     assertTrue(c1.poses.contains(t(0, 1)))
@@ -39,6 +41,9 @@ class PipeConnectionTest {
     val c3 = PipeConnection.empty[(Int, Int)]({ case (p, c) => map.get(p).foreach(_.setConnection(c)) }, p => p._2 == 3)
     val c4 = map.keys.foldLeft(c3) { case (c, p) => c add p }
     assertTrue(map.values.forall(_.connection == c4))
+    assertEquals(2, c4.outputs.size)
+    assertEquals(t(2, 3), c4.outputSorted(t(0, 0)).last)
+    assertEquals(t(1, 3), c4.outputSorted(t(3, 3)).last)
   }
 
   @inline
