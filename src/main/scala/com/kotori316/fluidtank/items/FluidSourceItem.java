574910a7a3a9a5ceb680c21d7c5eb607612ad0b9
Kotori316 <Kotori316@gmail.com>
2020-01-02 21:11:04 +0900

    Changed item name

--- src/main/scala/com/kotori316/fluidtank/items/FluidSourceItem.java
+++ src/main/scala/com/kotori316/fluidtank/items/FluidSourceItem.java
@@ -2,6 +2,7 @@ package com.kotori316.fluidtank.items;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
 import com.kotori316.fluidtank.FluidTank;
@@ -12,5 +13,13 @@ public class FluidSourceItem extends BlockItem {
         super(blockIn, builder);
         addPropertyOverride(new ResourceLocation(FluidTank.modID, "source_cheat"), (stack, world, entity) ->
             FluidSourceBlock.isCheatStack(stack) ? 1f : 0f);
+    }
+
+    @Override
+    public String getTranslationKey(ItemStack stack) {
+        if(!FluidSourceBlock.isCheatStack(stack)){
+            return "block.fluidtank.water_source";
+        }
+        return super.getTranslationKey(stack);
     }
 }
