98f07378ff589c46a85cb4892ed642f4b23656c4
Kotori316 <Kotori316@gmail.com>
2020-03-01 22:20:49 +0900

    Dynamic TierRecipe and Ingredient Serialize

--- src/main/scala/com/kotori316/fluidtank/recipes/RecipeSerializeHelper.scala
+++ src/main/scala/com/kotori316/fluidtank/recipes/RecipeSerializeHelper.scala
@@ -1,8 +1,12 @@
 package com.kotori316.fluidtank.recipes
 
 import com.google.gson.JsonObject
+import com.kotori316.fluidtank.tiles.Tiers
+import com.mojang.datafixers
+import com.mojang.datafixers.types.{DynamicOps, JsonOps}
 import net.minecraft.advancements.criterion.RecipeUnlockedTrigger
 import net.minecraft.data.{IFinishedRecipe, ShapedRecipeBuilder}
+import net.minecraft.item.crafting.Ingredient
 import net.minecraft.tags.Tag
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.common.crafting.conditions.{ICondition, NotCondition, TagEmptyCondition}
@@ -39,5 +43,44 @@ object RecipeSerializeHelper {
     var t: IFinishedRecipe = null
     c.build(p => t = p)
     t
+  }
+
+  import com.kotori316.fluidtank._
+
+  val TierRecipeSerializer: DynamicSerializable[TierRecipe] = new DynamicSerializable[TierRecipe] {
+    private[this] final val LOGGER = org.apache.logging.log4j.LogManager.getLogger(classOf[TierRecipe])
+
+    import cats._
+    import cats.implicits._
+
+    override def serialize[DataType](t: TierRecipe)(ops: DynamicOps[DataType]): datafixers.Dynamic[DataType] = {
+      val map = ops.emptyMap().pure[Id]
+        .map(d => ops.set(d, "tier", t.getTier.serialize(ops).getValue))
+        .map(d => if (t.getTier.hasTagRecipe) ops.set(d, "sub_item", t.getSubItems.serialize(ops).getValue) else d)
+
+      new datafixers.Dynamic[DataType](ops, map)
+    }
+
+    override def deserialize[DataType](d: datafixers.Dynamic[DataType]): TierRecipe = {
+      val recipeId = d.get("recipeId").asString().asScala.map(new ResourceLocation(_)).get
+      val tiers = d.get("tier").get().asScala
+        .map(DynamicSerializable[Tiers].deserialize)
+        .getOrElse(Tiers.Invalid)
+      val subItem = d.get("sub_item").get().asScala
+        .map(DynamicSerializable[Ingredient].deserialize)
+        .getOrElse(Ingredient.EMPTY)
+      if (subItem == Ingredient.EMPTY)
+        LOGGER.warn("Empty ingredient was loaded for {}, json: {}", recipeId, d.getValue)
+      LOGGER.debug("Serializer loaded {} from json for tier {}.", recipeId, tiers)
+      new TierRecipe(recipeId, tiers, subItem)
+    }
+  }
+
+  implicit val IngredientSerialize: DynamicSerializable[Ingredient] = new DynamicSerializable[Ingredient] {
+    override def serialize[DataType](t: Ingredient)(ops: DynamicOps[DataType]): datafixers.Dynamic[DataType] =
+      new datafixers.Dynamic(JsonOps.INSTANCE, t.serialize()).convert(ops)
+
+    override def deserialize[DataType](d: datafixers.Dynamic[DataType]): Ingredient =
+      Ingredient.deserialize(d.convert(JsonOps.INSTANCE).getValue)
   }
 }
