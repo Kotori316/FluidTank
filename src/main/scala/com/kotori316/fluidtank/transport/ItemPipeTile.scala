e2c96ce489b0e5939e6f27de1cccaa4dd5164ed6
Kotori316 <Kotori316@gmail.com>
2020-05-17 12:30:33 +0900

    Limit the transfer rate to 16 items/tick. Added cool time and faster when the transferring repeated.
    The pipe pulls stack some times(the number of destination) and send it for each dest. A dest can receive 16 items per a tick and the source send (16 * dest) items.

--- src/main/scala/com/kotori316/fluidtank/transport/ItemPipeTile.scala
+++ src/main/scala/com/kotori316/fluidtank/transport/ItemPipeTile.scala
@@ -5,6 +5,7 @@ import cats.data.OptionT
 import cats.implicits._
 import com.kotori316.fluidtank._
 import net.minecraft.item.ItemStack
+import net.minecraft.nbt.CompoundNBT
 import net.minecraft.tileentity.HopperTileEntity
 import net.minecraft.util.Direction
 import net.minecraft.util.math.BlockPos
@@ -16,43 +17,51 @@ import net.minecraftforge.items.{CapabilityItemHandler, IItemHandler, ItemHandle
 
 class ItemPipeTile extends PipeTileBase(ModObjects.ITEM_PIPE_TYPE) {
   val handler = new PipeItemHandler(this)
+  private[this] final var coolTime = ItemPipeTile.defaultCoolTime
+  private[this] final var repeated = 0
 
   override def tick(): Unit = if (!world.isRemote) {
     if (connection.isEmpty)
       makeConnection()
     import scala.jdk.CollectionConverters._
-    PipeBlock.FACING_TO_PROPERTY_MAP.asScala.toSeq.flatMap { case (direction, value) =>
-      if (getBlockState.get(value).isInput) {
-        val sourcePos = pos.offset(direction)
-        val c = findItemHandler(getWorld, sourcePos, direction)
-        c.toList
-      } else {
-        List.empty
-      }
-    }.foreach { case (f, sourcePos) =>
-      for {
-        p <- connection.outputs
-        (direction, pos) <- directions.map(f => f -> p.offset(f))
-        if pos != sourcePos
-        if getWorld.getBlockState(p).get(PipeBlock.FACING_TO_PROPERTY_MAP.get(direction)).isOutput
-        dest <- findItemHandler(getWorld, pos, direction).map { case (i, _) => i }.toList
-        if f != dest
-        index <- List.range(0, f.getSlots)
-        item <- f.extractItem(index, Int.MaxValue, true).pure[List] if !item.isEmpty
-      } {
-        val transferSimulate = ItemHandlerHelper.insertItem(dest, item, true)
-        if (!ItemStack.areItemsEqual(item, transferSimulate)) {
-          val result = ItemHandlerHelper.insertItem(dest, item, false)
-          f.extractItem(index, item.getCount - result.getCount, false)
+    coolTime -= 1
+    if (coolTime <= 0) {
+      val handlers = PipeBlock.FACING_TO_PROPERTY_MAP.asScala.toSeq.flatMap { case (direction, value) =>
+        if (getBlockState.get(value).isInput) {
+          val sourcePos = pos.offset(direction)
+          val c = findItemHandler(getWorld, sourcePos, direction)
+          c.toList
+        } else {
+          List.empty
         }
       }
+      handlers.foreach { case (f, sourcePos) =>
+        val func = (i: Int) => Option(f.extractItem(i, ItemPipeTile.transferItemCount, true)).filterNot(_.isEmpty).map(item => i -> item)
+        for {
+          p <- connection.outputs
+          (direction, pos) <- directions.map(f => f -> p.offset(f))
+          if pos != sourcePos
+          if getWorld.getBlockState(p).get(PipeBlock.FACING_TO_PROPERTY_MAP.get(direction)).isOutput
+          dest <- findItemHandler(getWorld, pos, direction).map { case (i, _) => i }.toList
+          if f != dest
+          (index, item) <- List.range(0, f.getSlots).collectFirst(Function.unlift(func))
+        } {
+          val transferSimulate = ItemHandlerHelper.insertItem(dest, item, true)
+          if (!ItemStack.areItemsEqual(item, transferSimulate)) {
+            val result = ItemHandlerHelper.insertItem(dest, item, false)
+            f.extractItem(index, item.getCount - result.getCount, false)
+          }
+        }
+      }
+      repeated = if (handlers.isEmpty) Math.max(repeated - 1, 0) else repeated + 1
+      coolTime = ItemPipeTile.getCoolTime(repeated)
     }
+
   }
 
   override def getCapability[T](cap: Capability[T], side: Direction): LazyOptional[T] = {
     Cap.asJava(
-      Cap.make(handler.asInstanceOf[T])
-        .filter(_ => cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+      CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.make[T](cap, handler)
         .filter(_ => side != null && getBlockState.get(PipeBlock.FACING_TO_PROPERTY_MAP.get(side)).is(PipeBlock.Connection.CONNECTED, PipeBlock.Connection.INPUT))
         .orElse(super.getCapability(cap, side).asScala)
     )
@@ -60,14 +69,35 @@ class ItemPipeTile extends PipeTileBase(ModObjects.ITEM_PIPE_TYPE) {
 
   def findItemHandler(world: World, pos: BlockPos, direction: Direction): OptionT[Eval, (IItemHandler, BlockPos)] = {
     def tileCap: OptionT[Eval, (IItemHandler, BlockPos)] = for {
-      t <- OptionT.fromOption[Eval](Option(world.getTileEntity(pos)))
+      t <- Cap.make(world.getTileEntity(pos))
       cap <- t.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, direction.getOpposite).asScala
     } yield cap -> pos
 
     def entityCap: OptionT[Eval, (IItemHandler, BlockPos)] = for {
-      inv <- OptionT(Eval.later(Option(HopperTileEntity.getInventoryAtPosition(world, pos))))
+      inv <- Cap.make(HopperTileEntity.getInventoryAtPosition(world, pos))
     } yield new InvWrapper(inv) -> pos
 
     tileCap orElse entityCap
+  }
+
+  override def read(compound: CompoundNBT): Unit = {
+    super.read(compound)
+    this.coolTime = compound.getInt("coolTime")
+    this.repeated = compound.getInt("repeated")
+  }
+
+  override def write(compound: CompoundNBT): CompoundNBT = {
+    compound.putInt("coolTime", this.coolTime)
+    compound.putInt("repeated", this.repeated)
+    super.write(compound)
+  }
+}
+
+object ItemPipeTile {
+  final val defaultCoolTime = 50
+  final val transferItemCount = 16
+
+  def getCoolTime(repeated: Int): Int = {
+    Math.max(1, defaultCoolTime - repeated / 2)
   }
 }
