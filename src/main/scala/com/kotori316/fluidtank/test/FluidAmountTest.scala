830c74ce086da59bde4edc80a4a9c12f3dd1ddfa
Kotori316 <Kotori316@gmail.com>
2020-06-13 21:08:42 +0900

    Empty and Adder FA Test

--- src/main/scala/com/kotori316/fluidtank/test/FluidAmountTest.scala
+++ src/main/scala/com/kotori316/fluidtank/test/FluidAmountTest.scala
@@ -3,6 +3,7 @@ package com.kotori316.fluidtank.test
 import cats.implicits._
 import com.kotori316.fluidtank._
 import net.minecraft.fluid.Fluids
+import net.minecraft.item.{ItemStack, Items}
 import net.minecraft.nbt.CompoundNBT
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
@@ -32,5 +33,46 @@ class FluidAmountTest {
     val a = FluidAmount(Fluids.WATER, 1000, Some(nbt1))
     val b = FluidAmount.BUCKET_WATER
     assertTrue(a =!= b, "Not eq due to nbt tag.")
+  }
+
+  @Test
+  def empty(): Unit = {
+    val tag = Some {
+      val a = new CompoundNBT()
+      a.putInt("e", 1)
+      a.putInt("4", 2)
+      a
+    }
+    assertAll(
+      () => assertTrue(FluidAmount.EMPTY.isEmpty),
+      () => assertTrue(FluidAmount.EMPTY.copy(nbt = tag).isEmpty),
+      () => assertTrue(FluidAmount(Fluids.EMPTY, 10000, None).isEmpty),
+      () => assertTrue(FluidAmount(Fluids.EMPTY, 10000, tag).isEmpty),
+      () => assertTrue(FluidAmount(Fluids.LAVA, 0, None).isEmpty),
+      () => assertTrue(FluidAmount.BUCKET_LAVA.setAmount(0).isEmpty),
+      () => assertTrue((FluidAmount.BUCKET_WATER - FluidAmount.BUCKET_WATER).isEmpty),
+      () => assertTrue(FluidAmount.fromItem(new ItemStack(Items.BUCKET)).isEmpty),
+    )
+  }
+
+  @Test
+  def adder(): Unit = {
+    {
+      val a = FluidAmount.BUCKET_WATER
+      assertEquals(FluidAmount.AMOUNT_BUCKET, a.amount)
+      assertEquals(FluidAmount.AMOUNT_BUCKET * 2, (a + a).amount)
+      assertEquals(FluidAmount.AMOUNT_BUCKET * 3, (a + a + a).amount)
+      assertEquals(FluidAmount.AMOUNT_BUCKET, a.amount)
+    }
+    locally {
+      val wl = FluidAmount.BUCKET_WATER + FluidAmount.BUCKET_LAVA
+      assertTrue(FluidAmount.BUCKET_WATER.setAmount(FluidAmount.AMOUNT_BUCKET * 2) === wl)
+      assertEquals(Fluids.WATER, wl.fluid)
+    }
+    locally {
+      val lw = FluidAmount.BUCKET_LAVA + FluidAmount.BUCKET_WATER
+      assertTrue(FluidAmount.BUCKET_WATER.setAmount(FluidAmount.AMOUNT_BUCKET * 2) === lw)
+      assertEquals(Fluids.LAVA, lw.fluid)
+    }
   }
 }
