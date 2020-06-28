63e26c6e3bf99c63f08e6c227c5a0c7a8dc0f4a1
Kotori316 <Kotori316@gmail.com>
2020-06-28 12:52:54 +0900

    Comment of GUI

--- src/main/scala/com/kotori316/fluidtank/tiles/CATScreen.java
+++ src/main/scala/com/kotori316/fluidtank/tiles/CATScreen.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
@@ -24,7 +23,9 @@ public class CATScreen extends ContainerScreen<CATContainer> {
     }
 
     @Override
+    @SuppressWarnings("deprecation")
     protected void func_230450_a_(MatrixStack matrixStack, float p_230450_2_, int p_230450_3_, int p_230450_4_) {
+        // background
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.getMinecraft().getTextureManager().bindTexture(resourceLocation);
         int i = (this.field_230708_k_ - this.xSize) / 2; // width
@@ -35,6 +36,7 @@ public class CATScreen extends ContainerScreen<CATContainer> {
     @Override
     @SuppressWarnings("NoTranslation")
     protected void func_230451_b_(MatrixStack matrixStack, int mouseX, int mouseY) {
+        //Foreground
         super.func_230451_b_(matrixStack, mouseX, mouseY);
 //        String s = this.func_231171_q_().getString(); // getTitle
 //        int x = this.xSize / 2 - this.field_230712_o_.getStringWidth(s) / 2; // font
