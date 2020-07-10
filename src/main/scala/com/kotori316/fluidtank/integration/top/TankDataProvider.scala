6d71ed3d82c888a9d9ef4a09a9005735baecc658
Kotori316 <Kotori316@gmail.com>
2020-07-11 00:26:29 +0900

    Re-Added support of TOP and updated JEI

--- src/main/scala/com/kotori316/fluidtank/integration/top/TankDataProvider.scala
+++ src/main/scala/com/kotori316/fluidtank/integration/top/TankDataProvider.scala
@@ -1,11 +1,12 @@
 package com.kotori316.fluidtank.integration.top
-/*
+
 import com.kotori316.fluidtank.integration.Localize._
 import com.kotori316.fluidtank.tiles.{FluidSourceTile, TileTankNoDisplay, TileTankVoid}
 import com.kotori316.fluidtank.{Config, FluidTank}
 import mcjty.theoneprobe.api.{IProbeHitData, IProbeInfo, IProbeInfoProvider, ProbeMode}
 import net.minecraft.block.BlockState
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.util.text.{StringTextComponent, TranslationTextComponent}
 import net.minecraft.world.World
 
 class TankDataProvider extends IProbeInfoProvider {
@@ -19,22 +20,21 @@ class TankDataProvider extends IProbeInfoProvider {
     val entity = world.getTileEntity(data.getPos)
     entity match {
       case v: TileTankVoid =>
-        probeInfo.text(s"Tier: ${v.tier.toString}")
+        probeInfo.text(new TranslationTextComponent(WAILA_TIER, v.tier))
       case tank: TileTankNoDisplay =>
-        val tier = s"Tier: ${tank.tier.toString}" //I18n.translateToLocalFormatted(WAILA_TIER, tank.tier.toString)
-        val fluid = s"Content: ${tank.connection.getFluidStack.map(_.getLocalizedName).getOrElse(FLUID_NULL)}" //I18n.translateToLocalFormatted(WAILA_CONTENT, tank.connection.getFluidStack.map(_.getLocalizedName).getOrElse(FLUID_NULL))
+        val tier = new TranslationTextComponent(WAILA_TIER, tank.tier) //I18n.translateToLocalFormatted(WAILA_TIER, tank.tier.toString)
+        val fluid = new TranslationTextComponent(WAILA_CONTENT, tank.connection.getFluidStack.map(_.getLocalizedName).getOrElse(FLUID_NULL))
         val list = if (tank.connection.hasCreative) Seq(tier, fluid)
         else {
-          val amount = s"Amount: ${Long.box(tank.connection.amount)}" //I18n.translateToLocalFormatted(WAILA_AMOUNT, Long.box(tank.connection.amount))
-          val capacity = s"Capacity: ${Long.box(tank.connection.capacity)}" //I18n.translateToLocalFormatted(WAILA_CAPACITY, Long.box(tank.connection.capacity))
-          val comparator = s"Level: ${Int.box(tank.getComparatorLevel)}" //I18n.translateToLocalFormatted(WAILA_COMPARATOR, Int.box(tank.getComparatorLevel))
+          val amount = new TranslationTextComponent(WAILA_AMOUNT, Long.box(tank.connection.amount))
+          val capacity = new TranslationTextComponent(WAILA_CAPACITY, Long.box(tank.connection.capacity))
+          val comparator = new TranslationTextComponent(WAILA_COMPARATOR, Int.box(tank.getComparatorLevel))
           Seq(tier, fluid, amount, capacity, comparator)
         }
         list.foreach(probeInfo.text)
       case source: FluidSourceTile =>
-        probeInfo.text(source.fluid.toString)
+        probeInfo.text(new StringTextComponent(source.fluid.toString))
       case _ =>
     }
   }
 }
-*/
