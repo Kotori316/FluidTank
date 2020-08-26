229793c4fd64a5345255496d78cadc2df193a7aa
Kotori316 <Kotori316@gmail.com>
2020-08-26 16:20:51 +0900

    SLP updated

--- src/main/scala/com/kotori316/fluidtank/package.scala
+++ src/main/scala/com/kotori316/fluidtank/package.scala
@@ -2,63 +2,15 @@ package com.kotori316
 
 import cats._
 import cats.data._
-import cats.implicits._
-import com.kotori316.scala_lib.util.Neighbor
+import com.kotori316.scala_lib.util.{CapConverter, Neighbor}
 import com.mojang.serialization.DataResult
 import net.minecraft.fluid.Fluid
 import net.minecraft.nbt.CompoundNBT
 import net.minecraft.util.Direction
 import net.minecraft.util.math.BlockPos
-import net.minecraftforge.common.capabilities.Capability
-import net.minecraftforge.common.util.{LazyOptional, NonNullSupplier}
 import net.minecraftforge.fluids.FluidStack
 
-package object fluidtank {
-
-  type Cap[T] = OptionT[Eval, T]
-
-  object Cap {
-    def make[T](obj: T): Cap[T] = {
-      OptionT.fromOption[Eval](Option(obj))
-    }
-
-    def asJava[A](cap: Cap[A]): LazyOptional[A] = {
-      cap.value.value.foldl(LazyOptional.empty[A]()) { case (_, a) => LazyOptional.of[A](() => a) }
-    }
-
-    def empty[A]: Cap[A] = {
-      OptionT.none
-    }
-  }
-
-  implicit class CapHelper[T](val capability: Capability[T]) extends AnyVal {
-    /**
-     * @tparam F dummy parameter to satisfy compiler. It should be type parameter of [[net.minecraftforge.common.capabilities.ICapabilityProvider#getCapability]].
-     */
-    def make[F](toCheckCapability: Capability[_], instance: T): Cap[F] = {
-      if (this.capability == toCheckCapability)
-        Cap.make[F](instance.asInstanceOf[F])
-      else
-        Cap.empty[F]
-    }
-  }
-
-  def transform0[T](cap: LazyOptional[T]): Eval[Option[T]] = Eval.always {
-    if (cap.isPresent) {
-      cap.orElseThrow(thrower).some
-    } else {
-      None
-    }
-  }
-
-  implicit class AsScalaLO[T](val cap: LazyOptional[T]) extends AnyVal {
-    def asScala: Cap[T] = OptionT(transform0[T](cap))
-  }
-
-  private val thrower: NonNullSupplier[AssertionError] = () =>
-    new AssertionError(
-      "LazyOptional has no content " +
-        "though it returned true when isPresent is called.")
+package object fluidtank extends CapConverter {
 
   implicit val showPos: Show[BlockPos] = pos => s"(${pos.getX}, ${pos.getY}, ${pos.getZ})"
   implicit val eqPos: Eq[BlockPos] = Eq.fromUniversalEquals
