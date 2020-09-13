a50d4bc1494e9b3d710365d8e9a41b64d3dcbc7d
Kotori316 <Kotori316@gmail.com>
2020-09-13 10:59:17 +0900

    Fixed a crash of potion rendering.

--- src/main/scala/com/kotori316/fluidtank/render/RenderTank.scala
+++ src/main/scala/com/kotori316/fluidtank/render/RenderTank.scala
@@ -10,6 +10,7 @@ import net.minecraft.client.render.block.entity.{BlockEntityRenderDispatcher, Bl
 import net.minecraft.client.render.{RenderLayer, VertexConsumerProvider}
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.tag.FluidTags
+import net.minecraft.util.math.Direction
 
 @Environment(EnvType.CLIENT)
 class RenderTank(d: BlockEntityRenderDispatcher) extends BlockEntityRenderer[TileTank](d) {
@@ -18,15 +19,29 @@ class RenderTank(d: BlockEntityRenderDispatcher) extends BlockEntityRenderer[Til
     MinecraftClient.getInstance.getProfiler.push("RenderTank")
     if (te.hasContent) {
       matrix.push()
-      val b = buffer.getBuffer(RenderLayer.getCutoutMipped)
       val tank = te.tank
       if (tank.box != null) {
-        val texture = RenderTank.textureName(te)
-        //        val texture = MinecraftClient.getInstance.getSpriteAtlas(PlayerContainer.BLOCK_ATLAS_TEXTURE).apply(resource)
-        val color = RenderTank.color(te)
+        if (tank.fluid.fluid != null) {
+          val texture = RenderTank.textureName(te)
+          //        val texture = MinecraftClient.getInstance.getSpriteAtlas(PlayerContainer.BLOCK_ATLAS_TEXTURE).apply(resource)
+          val color = RenderTank.color(te)
 
-        val value = Box.LightValue(light).overrideBlock(0 /*te.tank.fluid.fluid.getAttributes.getLuminosity(te.tank.fluid.toStack)*/)
-        tank.box.render(b, matrix, texture, color >> 24 & 0xFF, color >> 16 & 0xFF, color >> 8 & 0xFF, color >> 0 & 0xFF)(value)
+          val b = buffer.getBuffer(RenderLayer.getCutoutMipped)
+          val value = Box.LightValue(light).overrideBlock(0 /*te.tank.fluid.fluid.getAttributes.getLuminosity(te.tank.fluid.toStack)*/)
+          tank.box.render(b, matrix, texture, color >> 24 & 0xFF, color >> 16 & 0xFF, color >> 8 & 0xFF, color >> 0 & 0xFF)(value)
+        } else {
+          import alexiil.mc.lib.attributes.fluid.render.FluidRenderFace
+          val faces = new java.util.ArrayList[FluidRenderFace]
+          val x0 = tank.box.startX - tank.box.offX
+          val y0 = tank.box.startY
+          val z0 = tank.box.startZ - tank.box.offZ
+          val x1 = tank.box.endX + tank.box.offX
+          val y1 = tank.box.endY
+          val z1 = tank.box.endZ + tank.box.offZ
+          FluidRenderFace.appendCuboid(x0, y0, z0, x1, y1, z1, 1.0D, java.util.EnumSet.allOf(classOf[Direction]), faces)
+          faces.forEach(_.light = light)
+          tank.fluid.fluidVolume.render(faces, buffer, matrix)
+        }
       }
       matrix.pop()
     }
