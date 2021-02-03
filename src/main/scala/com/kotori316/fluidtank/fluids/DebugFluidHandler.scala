3bafb1c3b2618ae3a6ad8aef754ec31c1c9ccf7c
Kotori316 <Kotori316@gmail.com>
2021-02-04 00:33:04 +0900

    Changed log level

--- src/main/scala/com/kotori316/fluidtank/fluids/DebugFluidHandler.scala
+++ src/main/scala/com/kotori316/fluidtank/fluids/DebugFluidHandler.scala
@@ -1,25 +1,25 @@
 package com.kotori316.fluidtank.fluids
 
-import com.kotori316.fluidtank.ModObjects
-import com.kotori316.fluidtank.fluids.DebugFluidHandler.{LOGGER, MARKER}
+import com.kotori316.fluidtank.fluids.DebugFluidHandler.{LOGGER, LOG_LEVEL, MARKER}
+import com.kotori316.fluidtank.{ModObjects, Utils}
 import net.minecraftforge.fluids.FluidStack
 import net.minecraftforge.fluids.capability.IFluidHandler
 import net.minecraftforge.fluids.capability.templates.EmptyFluidHandler
-import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.{Level, LogManager}
 
 class DebugFluidHandler private() extends EmptyFluidHandler {
   override def fill(resource: FluidStack, action: IFluidHandler.FluidAction): Int = {
-    LOGGER.info(MARKER, "Fill {}, mode {}", resource: AnyRef, action: AnyRef)
+    LOGGER.log(LOG_LEVEL, MARKER, "Fill {}, mode {}", resource: AnyRef, action: AnyRef)
     super.fill(resource, action)
   }
 
   override def drain(resource: FluidStack, action: IFluidHandler.FluidAction): FluidStack = {
-    LOGGER.info(MARKER, "Drain {}, mode {}", resource: AnyRef, action: AnyRef)
+    LOGGER.log(LOG_LEVEL, MARKER, "Drain {}, mode {}", resource: AnyRef, action: AnyRef)
     super.drain(resource, action)
   }
 
   override def drain(maxDrain: Int, action: IFluidHandler.FluidAction): FluidStack = {
-    LOGGER.info(MARKER, "Drain amount {}, mode {}", maxDrain: Any, action: AnyRef)
+    LOGGER.log(LOG_LEVEL, MARKER, "Drain amount {}, mode {}", maxDrain: Any, action: AnyRef)
     super.drain(maxDrain, action)
   }
 
@@ -30,4 +30,5 @@ object DebugFluidHandler {
   final val INSTANCE = new DebugFluidHandler
   private final val LOGGER = LogManager.getLogger(classOf[DebugFluidHandler])
   private final val MARKER = ModObjects.MARKER_DebugFluidHandler
+  private final val LOG_LEVEL = if (Utils.isInDev) Level.INFO else Level.DEBUG
 }
