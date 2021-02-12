919a6458c2e4277eb91e13c53e7f856c02664dd4
Kotori316 <Kotori316@gmail.com>
2021-02-12 22:21:52 +0900

    Respect capacity in nbt tag

--- src/main/scala/com/kotori316/fluidtank/items/TankItemFluidHandler.scala
+++ src/main/scala/com/kotori316/fluidtank/items/TankItemFluidHandler.scala
@@ -32,7 +32,7 @@ class TankItemFluidHandler(item: ItemBlockTank, stack: ItemStack) extends IFluid
     }
     init()
     if (fluid.isEmpty) {
-      val amount = math.min(resource.getAmount, Utils.toInt(tiers.amount))
+      val amount = math.min(resource.getAmount, Utils.toInt(getCapacity))
       if (action.execute()) {
         fluid = resource.copy()
         fluid.setAmount(amount)
@@ -40,7 +40,7 @@ class TankItemFluidHandler(item: ItemBlockTank, stack: ItemStack) extends IFluid
       }
       amount
     } else if (fluid.isFluidEqual(resource)) {
-      val move = math.min(resource.getAmount, Utils.toInt(tiers.amount) - fluid.getAmount)
+      val move = math.min(resource.getAmount, Utils.toInt(getCapacity) - fluid.getAmount)
       if (action.execute()) {
         fluid.setAmount(fluid.getAmount + move)
         updateTag()
@@ -107,7 +107,7 @@ class TankItemFluidHandler(item: ItemBlockTank, stack: ItemStack) extends IFluid
     val tag = new CompoundNBT
     tag.put(TileTankNoDisplay.NBT_Tier, tiers.toNBTTag)
     val tankTag = new CompoundNBT
-    tankTag.putInt(TileTankNoDisplay.NBT_Capacity, Utils.toInt(tiers.amount))
+    tankTag.putInt(TileTankNoDisplay.NBT_Capacity, Utils.toInt(getCapacity))
     getFluid.write(tankTag)
     tag.put(TileTankNoDisplay.NBT_Tank, tankTag)
     tag
@@ -121,9 +121,11 @@ class TankItemFluidHandler(item: ItemBlockTank, stack: ItemStack) extends IFluid
     if (stack.getCount == 1) fluid else FluidStack.EMPTY
   }
 
-  override def getTankCapacity(tank: Int): Int = if (stack.getCount == 1) Utils.toInt(tiers.amount) else 0
+  override def getTankCapacity(tank: Int): Int = if (stack.getCount == 1) Utils.toInt(getCapacity) else 0
 
   override def isFluidValid(tank: Int, stack: FluidStack) = true
 
   def getFluid: FluidAmount = FluidAmount.fromStack(fluid)
+
+  def getCapacity: Long = if (tankNbt == null) tiers.amount else tankNbt.getLong(TileTankNoDisplay.NBT_Capacity)
 }
