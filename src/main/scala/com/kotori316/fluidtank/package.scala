1bfc70978eaa31de494d111a576cdf5325a8065e
Kotori316 <Kotori316@gmail.com>
2021-05-01 13:38:35 +0900

    Added show instance to FluidStack

--- src/main/scala/com/kotori316/fluidtank/package.scala
+++ src/main/scala/com/kotori316/fluidtank/package.scala
@@ -1,5 +1,7 @@
 package com.kotori316
 
+import java.lang.reflect.Field
+
 import cats._
 import cats.data._
 import com.kotori316.scala_lib.util.Neighbor
@@ -10,20 +12,28 @@ import net.minecraft.util.Direction
 import net.minecraft.util.math.BlockPos
 import net.minecraftforge.fluids.FluidStack
 
+import scala.util.chaining._
+
 package object fluidtank extends CapConverter {
 
-  implicit val showPos: Show[BlockPos] = pos => s"(${pos.getX}, ${pos.getY}, ${pos.getZ})"
-  implicit val eqPos: Eq[BlockPos] = Eq.fromUniversalEquals
-  implicit val hashFluid: Hash[Fluid] = Hash.fromUniversalHashCode
-  implicit val eqCompoundNbt: Eq[CompoundNBT] = Eq.fromUniversalEquals
-  implicit val hashFluidStack: Hash[FluidStack] = new Hash[FluidStack] {
+  implicit final val showPos: Show[BlockPos] = pos => s"(${pos.getX}, ${pos.getY}, ${pos.getZ})"
+  implicit final val eqPos: Eq[BlockPos] = Eq.fromUniversalEquals
+  implicit final val hashFluid: Hash[Fluid] = Hash.fromUniversalHashCode
+  implicit final val eqCompoundNbt: Eq[CompoundNBT] = Eq.fromUniversalEquals
+  implicit final val hashFluidStack: Hash[FluidStack] = new Hash[FluidStack] {
     override def hash(x: FluidStack): Int = x.##
 
     override def eqv(x: FluidStack, y: FluidStack): Boolean = x isFluidStackIdentical y
   }
+  private final val FluidStackAmountField: Field = classOf[FluidStack].getDeclaredField("amount").tap(_.setAccessible(true))
+  implicit final val showFluidStack: Show[FluidStack] = Show.show(stack => {
+    val amount = FluidStackAmountField.getInt(stack)
+    if (stack.hasTag) s"${stack.getTranslationKey}@$amount(${stack.getTag})"
+    else s"${stack.getTranslationKey}@$amount"
+  })
 
-  val directions: List[Direction] = Direction.values().toList
-  val evalExtractor: Eval ~> Id = new (Eval ~> Id) {
+  final val directions: List[Direction] = Direction.values().toList
+  final val evalExtractor: Eval ~> Id = new (Eval ~> Id) {
     override def apply[A](fa: Eval[A]): A = fa.value
   }
 
@@ -31,14 +41,14 @@ package object fluidtank extends CapConverter {
     origin.up, origin.down, origin.north, origin.east, origin.south, origin.west
   )
 
-  implicit class EitherToResult[A](private val either: Either[String, A]) extends AnyVal {
+  implicit final class EitherToResult[A](private val either: Either[String, A]) extends AnyVal {
     def toResult: DataResult[A] = either match {
       case Left(value) => DataResult.error(value)
       case Right(value) => DataResult.success(value)
     }
   }
 
-  implicit class IorToResult[A](private val ior: Ior[String, A]) extends AnyVal {
+  implicit final class IorToResult[A](private val ior: Ior[String, A]) extends AnyVal {
     def toResult: DataResult[A] = ior match {
       case Ior.Left(a) => DataResult.error(a)
       case Ior.Right(b) => DataResult.success(b)
