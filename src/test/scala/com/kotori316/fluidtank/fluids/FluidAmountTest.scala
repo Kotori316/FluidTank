5feac873b048b9594d077f649f20a2cd626f3913
Kotori316 <Kotori316@gmail.com>
2021-05-01 13:10:09 +0900

    Improved FluidAmountTest

--- src/test/scala/com/kotori316/fluidtank/fluids/FluidAmountTest.scala
+++ src/test/scala/com/kotori316/fluidtank/fluids/FluidAmountTest.scala
@@ -8,96 +8,163 @@ import net.minecraft.nbt.CompoundNBT
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.function.Executable
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 import scala.util.chaining._
 
-private[fluids] final class FluidAmountTest extends BeforeAllTest {
-  @Test
-  def equiv(): Unit = {
-    val a = FluidAmount(Fluids.WATER, 1000, None)
-    assertTrue(a === a, "Equal to itself")
-    val b = FluidAmount(Fluids.WATER, 1000, None)
-    assertTrue(a === b, "Eq")
-    assertEquals(a, b, "Eq2")
-    assertTrue(a fluidEqual b)
-    assertTrue(b fluidEqual a)
-  }
+object FluidAmountTest {
 
-  @Test
-  def eq2(): Unit = {
-    val a = FluidAmount(Fluids.WATER, 1000, None)
-    val b = FluidAmount(Fluids.WATER, 2000, None)
-    assertNotEquals(a, b)
-  }
+  def empties(): Array[FluidAmount] = fluidKeys().map(k => k.toAmount(0L))
 
-  @Test
-  def eq3(): Unit = {
-    val a = FluidAmount.EMPTY
-    val b = FluidAmount(Fluids.WATER, 0, None)
-    val c = FluidAmount.EMPTY.setAmount(2000)
-    assertNotEquals(a, b)
-    assertNotEquals(a, c)
-  }
-
-  @Test
-  def eqNbt(): Unit = {
-    val nbt1 = new CompoundNBT()
-    nbt1.putBoolean("b", true)
-    nbt1.putString("name", "name")
-    val nbt2 = new CompoundNBT()
-    nbt2.putBoolean("b", false)
-    nbt2.putString("name", "tag")
-    assertNotEquals(nbt1, nbt2)
-
-    val a = FluidAmount(Fluids.WATER, 1000, Some(nbt1))
-    val b = FluidAmount.BUCKET_WATER
-    val c = FluidAmount(Fluids.WATER, 1000, Some(nbt2))
-    assertAll(
-      () => assertTrue(a =!= b, s"Not eq due to nbt tag. $a, $b"),
-      () => assertTrue(a =!= c, s"Not eq due to nbt tag. $a, $c"),
+  def fluidKeys(): Array[FluidKey] = {
+    val nbt = Option(new CompoundNBT().tap(_.putInt("b", 6)))
+    Array(
+      FluidKey(Fluids.WATER, None), FluidKey(Fluids.LAVA, None), FluidKey(Fluids.EMPTY, None),
+      FluidKey(Fluids.WATER, nbt), FluidKey(Fluids.LAVA, nbt), FluidKey(Fluids.EMPTY, nbt),
     )
   }
 
-  @Test
-  def convertStackWithNBT(): Unit = {
-    val nbt1 = new CompoundNBT()
-    nbt1.putBoolean("b", true)
-    nbt1.putString("name", "name")
-    val a = FluidAmount(Fluids.WATER, 1000, Some(nbt1))
+  def fluidKeys2(): Array[Array[FluidKey]] = fluidKeys()
+    .combinations(2)
+    .map(_.toArray)
+    .toArray
 
-    assertTrue(a === FluidAmount.fromStack(a.toStack), s"NBT data${a -> FluidAmount.fromStack(a.toStack)}")
-  }
+  def fluidKeyAmount(): Array[Object] = for {
+    amount <- Array(0, 1000, 5000, Int.MaxValue, Int.MaxValue + 8L, Long.MaxValue)
+    fluid <- fluidKeys()
+  } yield Array(amount, fluid)
 
-  @Test
-  def convertStack(): Unit = {
-    val fa = for {
+  def fluidKey2Amount(): Array[Object] = for {
+    amount <- Array(0, 1000, 5000, Int.MaxValue, Int.MaxValue + 8L, Long.MaxValue)
+    keys <- fluidKeys2()
+  } yield Array(amount, keys(0), keys(1))
+
+  def stackFluids(): Array[FluidAmount] = {
+    val l = for {
       f <- List(FluidAmount.BUCKET_WATER, FluidAmount.BUCKET_LAVA)
       amount <- Iterator.iterate(1L)(_ * 10).take(10).filter(l => l < Int.MaxValue)
     } yield f.setAmount(amount)
-
-    assertAll(fa.map[Executable] { f =>
-      () => assertTrue(f === FluidAmount.fromStack(f.toStack), s"Reconvert $f, ${f.toStack}")
-    }: _*)
+    l.toArray
   }
 
-  @Test
-  def empty(): Unit = {
-    val tag = Some {
-      val a = new CompoundNBT()
-      a.putInt("e", 1)
-      a.putInt("4", 2)
-      a
+  object Equals extends BeforeAllTest {
+    @ParameterizedTest
+    @MethodSource(Array("com.kotori316.fluidtank.fluids.FluidAmountTest#fluidKeys"))
+    def equiv(key: FluidKey): Unit = {
+      val a = key.toAmount(1000)
+      val b = key.toAmount(1000)
+      assertAll(
+        () => assertTrue(a === a, "Equal to itself"),
+        () => assertTrue(a === b, "Eq"),
+        () => assertEquals(a, b, "Eq2"),
+        () => assertTrue(a fluidEqual b),
+        () => assertTrue(b fluidEqual a),
+      )
     }
-    assertAll(
-      () => assertTrue(FluidAmount.EMPTY.isEmpty),
-      () => assertTrue(FluidAmount.EMPTY.copy(nbt = tag).isEmpty),
-      () => assertTrue(FluidAmount(Fluids.EMPTY, 10000, None).isEmpty),
-      () => assertTrue(FluidAmount(Fluids.EMPTY, 10000, tag).isEmpty),
-      () => assertTrue(FluidAmount(Fluids.LAVA, 0, None).isEmpty),
-      () => assertTrue(FluidAmount.BUCKET_LAVA.setAmount(0).isEmpty),
-      () => assertTrue((FluidAmount.BUCKET_WATER - FluidAmount.BUCKET_WATER).isEmpty),
-      () => assertTrue(FluidAmount.fromItem(new ItemStack(Items.BUCKET)).isEmpty),
-    )
+
+    @ParameterizedTest
+    @MethodSource(Array("com.kotori316.fluidtank.fluids.FluidAmountTest#fluidKeys"))
+    def eq2(key: FluidKey): Unit = {
+      val a = key.toAmount(1000)
+      val b = key.toAmount(2000)
+      assertAll(
+        () => assertNotEquals(a, b),
+        () => assertTrue(a fluidEqual b),
+        () => assertTrue(b fluidEqual a),
+      )
+    }
+
+    @Test
+    def eq3(): Unit = {
+      val a = FluidAmount.EMPTY
+      val b = FluidAmount(Fluids.WATER, 0, None)
+      val c = FluidAmount.EMPTY.setAmount(2000)
+      assertNotEquals(a, b)
+      assertNotEquals(a, c)
+    }
+
+    @ParameterizedTest
+    @MethodSource(Array("com.kotori316.fluidtank.fluids.FluidAmountTest#fluidKey2Amount"))
+    def eq4(amount: Long, key1: FluidKey, key2: FluidKey): Unit = {
+      val a = key1.toAmount(amount)
+      val b = key2.toAmount(amount)
+      assertFalse(a === b, f"Eq of $a and $b")
+    }
+
+    @ParameterizedTest
+    @MethodSource(Array("com.kotori316.fluidtank.fluids.FluidAmountTest#fluidKeyAmount"))
+    def eqNbt(amount: Long, key: FluidKey): Unit = {
+      val nbt1 = new CompoundNBT()
+      nbt1.putBoolean("b", true)
+      nbt1.putString("name", "name")
+      val nbt2 = new CompoundNBT()
+      nbt2.putBoolean("b", false)
+      nbt2.putString("name", "tag")
+      assertNotEquals(nbt1, nbt2)
+
+      val a = key.copy(tag = Some(nbt1)).toAmount(amount)
+      val b = key.toAmount(amount)
+      val c = key.copy(tag = Some(nbt2)).toAmount(amount)
+      assertAll(
+        () => assertTrue(a =!= b, s"Not eq due to nbt tag. $a, $b"),
+        () => assertTrue(a =!= c, s"Not eq due to nbt tag. $a, $c"),
+        () => assertTrue(b =!= c, s"Not eq due to nbt tag. $b, $c"),
+      )
+    }
+  }
+
+  object Converts extends BeforeAllTest {
+
+    @ParameterizedTest
+    @MethodSource(Array("com.kotori316.fluidtank.fluids.FluidAmountTest#fluidKeys"))
+    def convertStackWithNBT(key: FluidKey): Unit = {
+      if (key.isEmpty) {
+        assertEquals(FluidAmount.EMPTY, FluidAmount.fromStack(key.toAmount(1000).toStack))
+        return
+      }
+      val nbt1 = new CompoundNBT()
+      nbt1.putBoolean("b", true)
+      nbt1.putString("name", "name")
+      val a = key.copy(tag = Some(nbt1)).toAmount(1000)
+
+      assertTrue(a === FluidAmount.fromStack(a.toStack), s"NBT data${a -> FluidAmount.fromStack(a.toStack)}")
+    }
+
+    @ParameterizedTest
+    @MethodSource(Array("com.kotori316.fluidtank.fluids.FluidAmountTest#stackFluids"))
+    def convertStack(f: FluidAmount): Unit = {
+      assertTrue(f === FluidAmount.fromStack(f.toStack), s"Reconvert $f, ${f.toStack}")
+    }
+
+  }
+
+  object Empties extends BeforeAllTest {
+    @Test
+    def empty(): Unit = {
+      val tag = Some {
+        val a = new CompoundNBT()
+        a.putInt("e", 1)
+        a.putInt("4", 2)
+        a
+      }
+      assertAll(
+        () => assertTrue(FluidAmount.EMPTY.isEmpty),
+        () => assertTrue(FluidAmount.EMPTY.copy(nbt = tag).isEmpty),
+        () => assertTrue(FluidAmount(Fluids.EMPTY, 10000, None).isEmpty),
+        () => assertTrue(FluidAmount(Fluids.EMPTY, 10000, tag).isEmpty),
+        () => assertTrue(FluidAmount(Fluids.LAVA, 0, None).isEmpty),
+        () => assertTrue(FluidAmount.BUCKET_LAVA.setAmount(0).isEmpty),
+        () => assertTrue((FluidAmount.BUCKET_WATER - FluidAmount.BUCKET_WATER).isEmpty),
+        () => assertTrue(FluidAmount.fromItem(new ItemStack(Items.BUCKET)).isEmpty),
+      )
+    }
+
+    @ParameterizedTest
+    @MethodSource(Array("com.kotori316.fluidtank.fluids.FluidAmountTest#empties"))
+    def emptyTest(maybeEmpty: FluidAmount): Unit = {
+      assertTrue(maybeEmpty.isEmpty)
+    }
   }
 
   @Test
