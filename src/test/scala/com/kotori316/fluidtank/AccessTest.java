fbc770dd54cf340906f0063ab61d9537bd57c1e7
Kotori316 <Kotori316@gmail.com>
2021-12-03 07:35:19 +0900

    Test reflection

--- src/test/scala/com/kotori316/fluidtank/AccessTest.java
+++ src/test/scala/com/kotori316/fluidtank/AccessTest.java
@@ -2,8 +2,11 @@ package com.kotori316.fluidtank;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -34,5 +37,32 @@ final class AccessTest extends BeforeAllTest {
     void configAccess() {
         assertTrue(Config.content().debug());
         assertFalse(Config.content().removeRecipe());
+    }
+
+    @Nested
+    class ShowFluidStack {
+        @Test
+        void water500() {
+            var stack = new FluidStack(Fluids.WATER, 500);
+            var str = com.kotori316.fluidtank.package$.MODULE$.showFluidStack().show(stack);
+            assertTrue(str.contains("500"), str);
+            assertTrue(str.contains("water"), str);
+        }
+
+        @Test
+        void lava1600() {
+            var stack = new FluidStack(Fluids.LAVA, 1600);
+            var str = com.kotori316.fluidtank.package$.MODULE$.showFluidStack().show(stack);
+            assertTrue(str.contains("1600"), str);
+            assertTrue(str.contains("lava"), str);
+        }
+
+        @Test
+        void empty300() {
+            var stack = new FluidStack(Fluids.EMPTY, 300);
+            var str = com.kotori316.fluidtank.package$.MODULE$.showFluidStack().show(stack);
+            assertTrue(str.contains("300"), str);
+            assertTrue(str.contains("air"), str);
+        }
     }
 }
