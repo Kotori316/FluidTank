55d0b1bd46e53f07e71b4d933f578ea9b6c2d898
Kotori316 <Kotori316@gmail.com>
2021-12-22 13:18:54 +0900

    Some tests [skip release]

---
+++ src/test/scala/com/kotori316/fluidtank/UtilsTest.java
@@ -0,0 +1,45 @@
+package com.kotori316.fluidtank;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import scala.Option;
+import scala.Some;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UtilsTest extends BeforeAllTest {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 100, Integer.MAX_VALUE - 1, Integer.MAX_VALUE, -1, Integer.MIN_VALUE + 1, Integer.MIN_VALUE,})
+    void inInt(int i) {
+        assertEquals(i, Utils.toInt(i));
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {1L + Integer.MAX_VALUE, 100L + Integer.MAX_VALUE, Long.MAX_VALUE - 1L, Long.MAX_VALUE})
+    void overMax(long l) {
+        assertEquals(Integer.MAX_VALUE, Utils.toInt(l));
+    }
+
+    @Test
+    void toJava1() {
+        var opt = Optional.of("a");
+        var scalaOpt = Option.apply("a");
+        assertEquals(opt, Utils.toJava(scalaOpt));
+    }
+
+    @Test
+    void toJava2() {
+        Optional<String> opt = Optional.empty();
+        Option<String> scalaOpt = Option.empty();
+        assertEquals(opt, Utils.toJava(scalaOpt));
+    }
+    @Test
+    void toJava3(){
+        Optional<String> opt = Optional.empty();
+        Option<String> scalaOpt = Some.apply(null);
+        assertEquals(opt, Utils.toJava(scalaOpt));
+    }
+}
--- src/test/scala/com/kotori316/fluidtank/tank/TiersTest.java
+++ src/test/scala/com/kotori316/fluidtank/tank/TiersTest.java
@@ -1,10 +1,16 @@
 package com.kotori316.fluidtank.tank;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.kotori316.fluidtank.BeforeAllTest;
+import com.kotori316.fluidtank.TankConfig;
 import com.kotori316.fluidtank.TankConstant;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -49,5 +55,28 @@ class TiersTest extends BeforeAllTest {
     @EnumSource(value = Tiers.class, names = {"CREATIVE", "VOID", "Invalid"})
     void noConfigEntry(Tiers tier) {
         assertTrue(TankConstant.config.capacity.get(tier.name()).isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("fieldNames")
+    void validFieldTest(String name, Field field) throws ReflectiveOperationException {
+        long viaField = field.getLong(TankConstant.config.capacity);
+        var viaMethod = TankConstant.config.capacity.get(name);
+        assertTrue(viaMethod.isPresent(), "Field(%s) has value but not declared in field.".formatted(name));
+        assertEquals(viaField, viaMethod.getAsLong());
+    }
+
+    static Stream<Object[]> fieldNames() {
+        var clazz = TankConfig.Capacity.class;
+        var fields = clazz.getFields();
+        return Arrays.stream(fields).map(f -> new Object[]{f.getName(), f});
+    }
+
+    @ParameterizedTest
+    @MethodSource("fieldNames")
+    void ignoreCase(String name, Field field) {
+        var lowerCase = TankConstant.config.capacity.get(name.toLowerCase());
+        var upperCase = TankConstant.config.capacity.get(name.toUpperCase());
+        assertEquals(lowerCase, upperCase, field.getName());
     }
 }
