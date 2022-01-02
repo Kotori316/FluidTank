0889c2b2470f7c69e29701c35d835b1f34767851
Kotori316 <Kotori316@gmail.com>
2022-01-02 12:42:01 +0900

    Added AE2 integration.

---
+++ src/main/scala/com/kotori316/fluidtank/integration/ae2/AEFluidInv.java
@@ -0,0 +1,73 @@
+package com.kotori316.fluidtank.integration.ae2;
+
+import alexiil.mc.lib.attributes.fluid.volume.FluidKeys;
+import appeng.api.config.Actionable;
+import appeng.api.networking.security.IActionSource;
+import appeng.api.stacks.AEFluidKey;
+import appeng.api.stacks.AEKey;
+import appeng.api.stacks.KeyCounter;
+import appeng.api.storage.MEStorage;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import scala.jdk.javaapi.OptionConverters;
+
+import com.kotori316.fluidtank.FluidAmount;
+import com.kotori316.fluidtank.tank.TileTank;
+
+@SuppressWarnings("ClassCanBeRecord")
+class AEFluidInv implements MEStorage {
+    private final TileTank tank;
+
+    AEFluidInv(TileTank tank) {
+        this.tank = tank;
+    }
+
+    @Override
+    public long insert(AEKey what, long amount, Actionable actionable, IActionSource source) {
+        FluidAmount fluidAmount = fromAEStack(what, amount);
+        FluidAmount filled = tank.connection().handler().fill(fluidAmount, actionable == Actionable.MODULATE, 0);
+        return filled.fluidVolume().amount().asLong(AEFluidKey.AMOUNT_BUCKET);
+    }
+
+    @Override
+    public long extract(AEKey what, long amount, Actionable actionable, IActionSource source) {
+        FluidAmount fluidAmount = fromAEStack(what, amount);
+        FluidAmount drained = tank.connection().handler().drain(fluidAmount, actionable == Actionable.MODULATE, 0);
+        return drained.fluidVolume().amount().asLong(AEFluidKey.AMOUNT_BUCKET);
+    }
+
+    @Override
+    public void getAvailableStacks(KeyCounter out) {
+        var amount = OptionConverters.toJava(tank.connection().getFluidStack());
+        amount.ifPresent(fluidAmount -> out.add(AEFluidKey.of(fluidAmount.fluid(), null /*No NBT*/),
+            Math.min(fluidAmount.fluidVolume().amount().asLong(AEFluidKey.AMOUNT_BUCKET), Long.MAX_VALUE - Integer.MAX_VALUE * 2L)));
+    }
+
+    @NotNull
+    @SuppressWarnings("UnstableApiUsage")
+    static FluidAmount fromAEStack(@Nullable AEKey what, long amount) {
+        if (what instanceof AEFluidKey fluidKey) {
+            return new FluidAmount(FluidKeys.get(fluidKey.getFluid()).withAmount(alexiil.mc.lib.attributes.fluid.amount.FluidAmount.of(amount, FluidConstants.BUCKET)));
+        } else {
+            return FluidAmount.EMPTY();
+        }
+    }
+
+    @Override
+    public Component getDescription() {
+        return this.tank.getName();
+    }
+
+    @Override
+    public boolean isPreferredStorageFor(AEKey what, IActionSource source) {
+        if (what instanceof AEFluidKey fluidKey) {
+            var tankKey = tank.connection().fluidType();
+            if (tankKey.fluid().isSame(fluidKey.getFluid())) {
+                return !fluidKey.hasTag(); // Currently, NBT tag is not supported.
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/scala/com/kotori316/fluidtank/integration/ae2/TankAE2Plugin.java
@@ -0,0 +1,45 @@
+package com.kotori316.fluidtank.integration.ae2;
+
+import appeng.api.IAEAddonEntrypoint;
+import appeng.api.networking.security.IActionSource;
+import appeng.api.storage.IStorageMonitorableAccessor;
+import appeng.api.storage.MEStorage;
+import net.fabricmc.loader.api.FabricLoader;
+
+import com.kotori316.fluidtank.ModTank;
+import com.kotori316.fluidtank.tank.TileTank;
+
+public final class TankAE2Plugin implements IAEAddonEntrypoint {
+    // public static final ResourceLocation LOCATION = new ResourceLocation(ModTank.modID, "attach_ae2");
+
+    @Override
+    public void onAe2Initialized() {
+        if (FabricLoader.getInstance().isModLoaded("ae2")) {
+            CapHandler.event();
+        }
+    }
+
+    static class CapHandler {
+        public static void event() {
+            IStorageMonitorableAccessor.SIDED.registerForBlockEntities((blockEntity, context) -> {
+                if (blockEntity instanceof TileTank tank) return new AEConnectionCapabilityProvider(tank);
+                else return null;
+            }, ModTank.Entries.TANK_BLOCK_ENTITY_TYPE, ModTank.Entries.VOID_BLOCK_ENTITY_TYPE, ModTank.Entries.CREATIVE_BLOCK_ENTITY_TYPE);
+        }
+    }
+}
+
+class AEConnectionCapabilityProvider implements IStorageMonitorableAccessor {
+    private final TileTank tank;
+    private AEFluidInv aeFluidInv;
+
+    public AEConnectionCapabilityProvider(TileTank tank) {
+        this.tank = tank;
+    }
+
+    @Override
+    public MEStorage getInventory(IActionSource iActionSource) {
+        if (aeFluidInv == null) aeFluidInv = new AEFluidInv(tank);
+        return aeFluidInv;
+    }
+}
