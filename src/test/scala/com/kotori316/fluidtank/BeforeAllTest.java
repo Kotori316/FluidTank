1ed5942e8e9ecef29b031239ce41ac5b330b254a
Kotori316 <Kotori316@gmail.com>
2022-06-08 15:39:19 +0900

    Add mock for ForgeRegistry to create ItemStack instance.

--- src/test/scala/com/kotori316/fluidtank/BeforeAllTest.java
+++ src/test/scala/com/kotori316/fluidtank/BeforeAllTest.java
@@ -1,6 +1,11 @@
 package com.kotori316.fluidtank;
 
+import java.lang.reflect.Field;
+import java.util.Optional;
+
 import com.electronwill.nightconfig.core.CommentedConfig;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityManager;
@@ -9,7 +14,11 @@ import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.ForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistry;
 import org.junit.jupiter.api.BeforeAll;
+import sun.misc.Unsafe;
 
 import com.kotori316.testutil.MCTestInitializer;
 
@@ -17,6 +26,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 public abstract class BeforeAllTest {
     @BeforeAll
@@ -27,6 +38,7 @@ public abstract class BeforeAllTest {
     public static synchronized void setup() {
         setConfig();
         mockCapability();
+        mockRegistries();
     }
 
     @SuppressWarnings("unchecked")
@@ -49,6 +61,35 @@ public abstract class BeforeAllTest {
         assertNotNull(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY);
         assertNotNull(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY);
         assertNotNull(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY);
+    }
+
+    private static void mockRegistries() {
+        try {
+            mockRegistry(ForgeRegistries.ITEMS, ForgeRegistries.class.getDeclaredField("ITEMS"));
+            mockRegistry(ForgeRegistries.BLOCKS, ForgeRegistries.class.getDeclaredField("BLOCKS"));
+            mockRegistry(ForgeRegistries.FLUIDS, ForgeRegistries.class.getDeclaredField("FLUIDS"));
+        } catch (ReflectiveOperationException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    @SuppressWarnings({"unchecked", "deprecation"})
+    private static <T> void mockRegistry(IForgeRegistry<T> registry, Field field) throws ReflectiveOperationException {
+        var wrapperGetter = ForgeRegistry.class.getDeclaredMethod("getWrapper");
+        wrapperGetter.setAccessible(true);
+        var wrapper = (Registry<T>) wrapperGetter.invoke(registry);
+
+        var s = spy(registry);
+        when(s.getDelegate((T) any())) // Return: Optional<Holder.Reference<V>>
+            .thenAnswer(invocation -> {
+                T arg = invocation.getArgument(0);
+                return Optional.of(Holder.Reference.createIntrusive(wrapper, arg));
+            });
+
+        var theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+        theUnsafe.setAccessible(true);
+        var unsafe = (Unsafe) theUnsafe.get(null);
+        unsafe.putObject(unsafe.staticFieldBase(field), unsafe.staticFieldOffset(field), s);
     }
 
     private static void setConfig() {
