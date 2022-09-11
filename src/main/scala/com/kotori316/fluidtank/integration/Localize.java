0696adda5c8f843863c28fe7d26ae9a2d4135fdf
Kotori316 <Kotori316@gmail.com>
2022-09-11 09:13:08 +0900

    Fixed jade plugin

--- src/main/scala/com/kotori316/fluidtank/integration/Localize.java
+++ src/main/scala/com/kotori316/fluidtank/integration/Localize.java
@@ -9,8 +9,8 @@ public class Localize {
     public static final String AMOUNT = "fluidtank.waila.amount";
     public static final String CAPACITY = "fluidtank.waila.capacity";
     public static final String COMPARATOR = "fluidtank.waila.comparator";
-    public static final String WAILA_TANK_INFO = "fluidtank.waila.tank_info";
-    public static final String WAILA_SHORT_INFO = "fluidtank.waila.short_info";
+    public static final String WAILA_TANK_INFO = "fluidtank.tank_info";
+    public static final String WAILA_SHORT_INFO = "fluidtank.short_info";
     public static final String WAILA_SHORT = "fluidtank.waila.short";
     public static final String WAILA_SUPPLIER = "fluidtank.waila.supplier";
     public static final String FLUID_NULL = "None";
