9e1c9ebc8a163a4abf8e556f50e5015481cc2609
Kotori316 <Kotori316@gmail.com>
2022-11-04 14:33:23 +0900

    GasListHandler test

--- src/test/scala/com/kotori316/fluidtank/gametest/MekanismGasTest.scala
+++ src/test/scala/com/kotori316/fluidtank/gametest/MekanismGasTest.scala
@@ -1,15 +1,17 @@
 package com.kotori316.fluidtank.gametest
 
+import cats.data.Chain
 import com.kotori316.fluidtank.FluidTank
 import com.kotori316.fluidtank.fluids.Tank
 import com.kotori316.fluidtank.gametest.MekanismGasTest.BATCH
-import com.kotori316.fluidtank.integration.mekanism_gas.{GasAmount, GasTankHandler}
+import com.kotori316.fluidtank.integration.mekanism_gas._
 import com.kotori316.testutil.GameTestUtil.EMPTY_STRUCTURE
 import mekanism.api.chemical.gas.{Gas, GasStack}
 import mekanism.api.{AutomationType, Action => MekanismAction}
 import mekanism.common.registries.MekanismGases
 import net.minecraft.gametest.framework.{AfterBatch, BeforeBatch, GameTest, GameTestHelper}
 import net.minecraft.server.level.ServerLevel
+import net.minecraftforge.fluids.capability.IFluidHandler
 import net.minecraftforge.gametest.{GameTestHolder, PrefixGameTestTemplate}
 import org.junit.jupiter.api.Assertions._
 
@@ -118,6 +120,64 @@ class MekanismGasTest {
     val drained = tank.extract(1500, MekanismAction.SIMULATE, AutomationType.EXTERNAL)
 
     assertEquals(new GasStack(stack, 1500), drained)
+
+    helper.succeed()
+  }
+
+  @GameTest(template = EMPTY_STRUCTURE, batch = BATCH)
+  def setStack1(helper: GameTestHelper): Unit = {
+    val handler = GasTankHandler(4000)
+    handler.setStack(GasAmount(MekanismGases.OXYGEN.get(), 1000).toStack)
+    assertEquals(GasAmount(MekanismGases.OXYGEN.get(), 1000), handler.getTank.genericAmount)
+
+    helper.succeed()
+  }
+
+  @GameTest(template = EMPTY_STRUCTURE, batch = BATCH)
+  def setStack2(helper: GameTestHelper): Unit = {
+    val handler = GasTankHandler(4000)
+    handler.fill(GasAmount(MekanismGases.ANTIMATTER.get(), 1000), MekanismAction.EXECUTE)
+    handler.setStack(GasAmount(MekanismGases.OXYGEN.get(), 1000).toStack)
+    assertEquals(GasAmount(MekanismGases.OXYGEN.get(), 1000), handler.getTank.genericAmount)
+
+    helper.succeed()
+  }
+
+  @GameTest(template = EMPTY_STRUCTURE, batch = BATCH)
+  def list1Simulate(helper: GameTestHelper): Unit = {
+    val handler1 = GasTankHandler(4000)
+    val handler2 = GasTankHandler(4000)
+    val list = new GasListHandler(Chain(handler1, handler2))
+    val filled = list.fill(GasAmount(MekanismGases.OXYGEN.get(), 6000), IFluidHandler.FluidAction.SIMULATE)
+    assertEquals(GasAmount(MekanismGases.OXYGEN.get(), 6000), filled)
+    assertTrue(handler1.isEmpty, "Simulation must not change the content. %s".formatted(handler1))
+    assertTrue(handler2.isEmpty, "Simulation must not change the content. %s".formatted(handler2))
+
+    helper.succeed()
+  }
+
+  @GameTest(template = EMPTY_STRUCTURE, batch = BATCH)
+  def list1Execute(helper: GameTestHelper): Unit = {
+    val handler1 = GasTankHandler(4000)
+    val handler2 = GasTankHandler(4000)
+    val list = new GasListHandler(Chain(handler1, handler2))
+    val filled = list.fill(GasAmount(MekanismGases.OXYGEN.get(), 6000), IFluidHandler.FluidAction.EXECUTE)
+    assertEquals(GasAmount(MekanismGases.OXYGEN.get(), 6000), filled)
+    assertEquals(GasAmount(MekanismGases.OXYGEN.get(), 4000), handler1.getTank.genericAmount)
+    assertEquals(GasAmount(MekanismGases.OXYGEN.get(), 2000), handler2.getTank.genericAmount)
+
+    helper.succeed()
+  }
+
+  @GameTest(template = EMPTY_STRUCTURE, batch = BATCH)
+  def list1Set(helper: GameTestHelper): Unit = {
+    val handler1 = GasTankHandler(4000)
+    val handler2 = GasTankHandler(4000)
+    val list = new GasListHandler(Chain(handler1, handler2))
+    list.setChemicalInTank(0, GasAmount(MekanismGases.OXYGEN.get(), 6000).toStack)
+
+    assertEquals(GasAmount(MekanismGases.OXYGEN.get(), 4000), handler1.getTank.genericAmount)
+    assertEquals(GasAmount(MekanismGases.OXYGEN.get(), 2000), handler2.getTank.genericAmount)
 
     helper.succeed()
   }
